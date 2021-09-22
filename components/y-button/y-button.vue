e11523626f16cc4056aea89448532a43874a7564
zhuzhengjian <aibyanyu@qq.com>
2021-09-22 09:10:02 +0800

    初始化项目

---
+++ components/y-button/y-button.vue
@@ -0,0 +1,28 @@
+<template>
+	<button class="">
+		<slot>按钮</slot>
+	</button>
+</template>
+
+<script>
+	/**
+	 * YButton 按钮组件
+	 * @description 这是一个自定义的按钮
+	 * @tutorial https://www.28yanyu.cn
+	 */
+	export default {
+		name:"y-button",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	// 引入公共样式文件
+	@import "../../style/common.scss";
+	// 自定义的样式库
+	@import "./button.scss";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	// 引入公共的css变量
+	.test{
+	}
+</style>
